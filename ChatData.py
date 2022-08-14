593498fc3da0dacca86dc2bc6697bfb75b4dc3fa
JMcFcode <joelattack@gmail.com>
2022-08-14 12:23:05 +0100

    WhatsApp Chats:
    Added Summary of chat. A summary of what each participant did is also saved in the class.
    
    Added support for group chats.
    
    Added in Pie chart for number of messages.

--- ChatData.py
+++ ChatData.py
@@ -10,12 +10,14 @@ import numpy as np
 import re
 import time
 import os
+import string
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime, timedelta
 from collections import Counter
 from copy import copy
+from functools import reduce
 
 import chat_config
 
@@ -29,12 +31,18 @@ class ChatData:
 
     @staticmethod
     def parse_data(filename: str):
-        with open(os.getcwd() + '//' + filename) as f:
+        with open(os.getcwd() + '//' + filename, 'r', encoding='utf8') as f:
             lines = f.readlines()
         new_lines = []
         lines.reverse()
+
         for i in range(len(lines)):
             line = lines[i]
+            line = line.replace('\u200e', '')
+            line = line.replace('\u202a', '')
+            line = line.replace('\u202c', '')
+            line = line.replace('\xa0', '')
+            line = line.replace('\n', '')
             dt = line[0: 23].strip()
             if len(re.sub('\D', "", dt)) == 14 and dt.count('/') == 2 and dt.count(':') == 2:
                 new_lines.append(line)
@@ -58,14 +66,13 @@ class ChatData:
             dt = re.sub('\D', "", dt)
             dt = datetime.strptime(dt, '%d%m%Y%H%M%S')
             dt_list.append(dt)
+
         raw_txt['Date'] = dt_list
-
         raw_txt['Msg'] = [line[23:] for line in raw_txt[0]]
-
-        raw_txt['Sender'] = [line[0: line.index(':')] for line in raw_txt['Msg']]
+        raw_txt['Sender'] = [line[0: line.index(':')] if ':' in line else 'SYSTEM'
+                             for line in raw_txt['Msg']]
         raw_txt['Sender'] = [string.strip() for string in raw_txt['Sender']]
-
-        raw_txt['Message'] = [line[line.index(':') + 2:] for line in raw_txt['Msg']]
+        raw_txt['Message'] = [line[line.index(':') + 2:] if ':' in line else line for line in raw_txt['Msg']]
 
         data_df = raw_txt[['Date', 'Sender', 'Message']]
         data_df = data_df.iloc[1:, :].reset_index(drop=True)
@@ -98,7 +105,7 @@ class ChatData:
         ax[ax_val].set_xticks(numbers)
         ax[ax_val].set_xticklabels(labels)
         ax[ax_val].set_xlim(0, 24)
-        ax[ax_val].hist(hour_list, bins=24 * 4, label=label, alpha=0.5)
+        ax[ax_val].hist(hour_list, bins=24 * 4, label=label, alpha=0.5, histtype='bar', fill=True)
 
     def plot_hist(self, df: pd.DataFrame):
         """
@@ -114,17 +121,31 @@ class ChatData:
         ax[0].set_ylabel('Number of Messages')
         ax[0].legend(loc='best')
 
-        list_month = df['Date'].dt.month.to_list()
-        month_df = self.categorical_sort(list_cat=list_month,
-                                         mapping=chat_config.month_map, label='Month')
-        ax[1].bar(month_df['Month'], month_df['Count'], label=None)
+        month_df_list = []
+        for name in self.names:
+            df_person = df[df['Sender'] == name]
+            list_month = df_person['Date'].dt.month.to_list()
+            month_df = self.categorical_sort(list_cat=list_month,
+                                             mapping=chat_config.month_map, label='Month')
+            month_df.rename(columns={'Count': name}, inplace=True)
+            month_df.drop(columns=['m'], inplace=True)
+            month_df_list.append(month_df)
+        all_months_df = reduce(lambda left, right: pd.merge(left, right, on=['Month'], how='outer'), month_df_list)
+        all_months_df.plot.bar(x='Month', stacked=True, ax=ax[1])
         ax[1].set_xlabel('Month')
         ax[1].set_ylabel('Number of Messages')
 
-        list_day = df['Date'].apply(lambda x: x.strftime('%A'))
-        day_df = self.categorical_sort(list_cat=list_day,
-                                       mapping=chat_config.day_map, label='Day')
-        ax[2].bar(day_df['Day'], day_df['Count'], label=None)
+        day_df_list = []
+        for name in self.names:
+            df_person = df[df['Sender'] == name]
+            list_day = df_person['Date'].apply(lambda x: x.strftime('%A'))
+            day_df = self.categorical_sort(list_cat=list_day,
+                                           mapping=chat_config.day_map, label='Day')
+            day_df.rename(columns={'Count': name}, inplace=True)
+            day_df.drop(columns=['m'], inplace=True)
+            day_df_list.append(day_df)
+        all_days_df = reduce(lambda left, right: pd.merge(left, right, on=['Day'], how='outer'), day_df_list)
+        all_days_df.plot.bar(x='Day', stacked=True, ax=ax[2])
         ax[2].set_xlabel('Day')
         ax[2].set_ylabel('Number of Messages')
 
@@ -194,7 +215,7 @@ class ChatData:
         Calculate how many words sent in a rolling average period.
         """
         res_dict = {}
-        for date in self.date_list:
+        for date in self.date_list_mov_avg:
             temp_df = df[(df['Date'] > date) & (df['Date'] < date + timedelta(chat_config.mov_avg_period))]
             word_count = self.calc_data_amount(df=temp_df)
             res_dict[date] = word_count
@@ -208,7 +229,7 @@ class ChatData:
         Calculate the average reply time in a rolling average period.
         """
         res_dict = {}
-        for date in self.date_list:
+        for date in self.date_list_mov_avg:
             temp_df = reply_df[(reply_df['DateTime'] > date) & (reply_df['DateTime']
                                                                 < date + timedelta(chat_config.mov_avg_period))]
             avg_reply = np.mean(temp_df['Reply_Time'])
@@ -259,16 +280,64 @@ class ChatData:
             print(f'{name} has average reply time of {round(float(mean), 1)} minutes and standard deviation of'
                   f' {round(float(temp_std_dev), 1)} minutes \n ')
 
+    @staticmethod
+    def summary_all(data_df: pd.DataFrame, print_logs: bool = True) -> dict:
+        """
+        Summary of all the whole chat.
+        """
+        list_active = [1 if date in data_df['Date'].dt.date.values else 0 for date in
+                       pd.date_range(start=data_df['Date'].values[0], end=data_df['Date'].values[-1]).date]
+
+        days_active = sum(list_active)
+        days_inactive = len(data_df) - days_active
+
+        string_data = data_df['Message'].str.cat(sep=' ')
+        num_words = sum([i.strip(string.punctuation).isalpha() for i in string_data.split()])
+
+        dict_out = {'Number of Messages': len(data_df),
+                    'Word Count': num_words,
+                    'Period': f"{pd.to_datetime(data_df['Date'].values[0]).date()} -> "
+                              f"{pd.to_datetime(data_df['Date'].values[-1]).date()}",
+                    'Days Active': days_active,
+                    'Days Inactive': days_inactive}
+        dict_out['Word_per_message'] = dict_out['Word Count'] / dict_out['Number of Messages']
+
+        if print_logs:
+            print('\n')
+            for key, values in dict_out.items():
+                print(f'{key}: {values}')
+            print('\n')
+        return dict_out
+
+    def draw_pie(self):
+        fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+
+        ax.set_title('Number of Messages')
+        list_messages = [self.dict_summary[name]['Number of Messages'] for name in self.names]
+        ax.pie(list_messages, labels=self.names, startangle=90)
+
+        plt.show()
+
     def run(self):
         txt = self.parse_data(filename=self.filename)
         data_df = self.into_df(txt=txt)
-        self.data_df = data_df
-        self.names = pd.unique(data_df['Sender'])
+        if 'SYSTEM' in data_df['Sender'].values:
+            self.full_group_df = data_df
+            data_df = data_df[data_df['Sender'] != 'SYSTEM']
+        else:
+            self.full_group_df = None
 
+        self.names = pd.unique(data_df['Sender'])
         start_date = data_df['Date'].iloc[0]
-        self.date_list = pd.date_range(start_date, datetime.today(), freq=str(chat_config.mov_avg_freq) + 'D')
+        self.date_list_mov_avg = pd.date_range(start_date, datetime.today(), freq=str(chat_config.mov_avg_freq) + 'D')
+
+        self.dict_summary_all = self.summary_all(data_df=data_df)
 
         self.dict_data = {'All': data_df}
+        self.data_df = data_df
+        self.dict_summary = {name: self.summary_all(data_df=data_df[data_df['Sender'] == name], print_logs=False)
+                             for name in self.names}
+
         for name in self.names:
             self.dict_data[name] = data_df[data_df['Sender'] == name]
 
@@ -280,3 +349,4 @@ if __name__ == '__main__':
 
     chat_class.plot_hist(df=output)
     chat_class.plot_time_series()
+    chat_class.draw_pie()
--- README.md
+++ README.md
@@ -9,12 +9,14 @@ import numpy as np
 import re
 import time
 import os
+import string
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime, timedelta
 from collections import Counter
 from copy import copy
+from functools import reduce
 ```
 All are standard packages installed with anaconda.
 
@@ -27,18 +29,16 @@ Specify relative path to file.
 WhatsApp file must be downloaded without images/docs in a .txt format.
 
 ### Currently Supports:
-- Reading in data from whatsapp .txt file (two person chats)
-- Looking at histogram of messages sent via hour and via month.
+- Reading in data from whatsapp .txt file (two person chats and group chats)
+- Looking at histogram of messages sent via hour and via month. Stacked to show what each participant did.
 - Looking at reply statistics, by sender. Prints out averages and standard deviations
+- View Pie chart that shows number of messages sent.
 - Looking at Time Series of reply time and number of words sent over a rolling average period.
+- Summary of chat data printed to console.
 
 ### Future plans:
 Add NLP Sentiment and Emoji Analysis.
 
-Add support for group-chats.
-
 Add wordcloud
-
-Add Summary of chat the includes useful statistics.
 
 Add support for other messaging services.
